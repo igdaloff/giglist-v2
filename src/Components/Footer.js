87a32bfa0d01a51f27f66e2287e8eded49e4bbb3
Nathan Igdaloff <louis.nathan@gmail.com>
2022-04-18 20:59:28 -0700

    fix typo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function Footer(){
 	return(
 		<footer className="absolute max-w-xl left-0 right-0 bottom-0 m-auto p-8 text-s text-gray-400">
-			<p className="">Giglist makes it easy to discover live shows by showing a random nearby concert happenning soon, and a song to help you decide if you’re into it.</p>
+			<p className="">Giglist makes it easy to discover live shows by showing a random nearby concert happening soon, and a song to help you decide if you’re into it.</p>
 			<p className="mt-8">Made for you by <a className="underline" href="igdaloff.com">Nathan Igdaloff</a> | <a className="underline" href="">About</a></p>			
 		</footer>
 	)
