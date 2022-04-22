a0827d65fa92b45f6e0406caf971c79c53622254
Nathan Igdaloff <louis.nathan@gmail.com>
2022-04-22 13:21:13 -0700

    move songkick data to separate file for cleanliness

--- src/Components/CitySelect.js
+++ src/Components/CitySelect.js
@@ -6,15 +6,15 @@ export function CitySelect(props) {
 	const {
 		city,
 		setCity,
-		cityOptions
+		songkickCityData
 	} = props;
 	
-  const cityData = props.cityOptions[props.city];
+  const cityData = props.songkickCityData[props.city];
 
 	return (
 		<div className="city-select relative inline">
 			<select className="bg-gray-600 hover:bg-gray-700 text-2xl sm:text-3xl py-2 px-3 mt-2 font-light rounded-sm border-gray-600 hover:border-gray-700 cursor-pointer" value={city} onChange={ (event) => props.setCity(event.target.value) }>
-				{Object.values(props.cityOptions).map((option) => (
+				{Object.values(props.songkickCityData).map((option) => (
 					<option key={option.value} value={option.value}>{option.label}</option>
 			  ))}
 			</select>		
---
+++ src/Data/songkickCityData.js
@@ -0,0 +1,169 @@
+const songkickCityData = {
+  "amsterdam": {
+    label: "Amsterdam",
+    value: "amsterdam",    
+    id: 31366
+  },
+  "austin": {
+    label: "Austin",
+    value: "austin",    
+    id: 9179
+  },
+  "barcelona": {
+    label: "Barcelona",
+    value: "barcelona",    
+    id: 28714
+  },
+  "berlin": {
+    label: "Berlin",
+    value: "berlin",    
+    id: 28443
+  },
+  "boston": {
+    label: "Boston",
+    value:"boston",
+    id: 18842
+  },
+  "charlotte": {
+    label: "Charlotte",
+    value: "charlotte",    
+    id: 13579
+  },
+  "chicago": {
+    label: "Chicago",
+    value: "chicago",   
+    id: 9426
+  },
+  "columbus": {
+    label: "Columbus",
+    value: "columbus",   
+    id: 9480
+  },
+  "dallas": {
+    label: "Dallas",
+    value: "dallas",    
+    id: 35129
+  },
+  "denver": {
+    label: "Denver",
+    value: "denver",    
+    id: 6404
+  },
+  "detroit": {
+    label: "Detroit",
+    value: "detroit",    
+    id: 18073
+  },
+  "houston": {
+    label: "Houston",
+    value: "houston",   
+    id: 15073
+  },
+  "indianapolis": {
+    label: "Indianapolis",
+    value: "indianapolis",    
+    id: 25521
+  },
+  "jacksonville": {
+    label: "Jacksonville",
+    value: "jacksonville",    
+    id: 24578
+  },
+  "las-vegas": {
+    label: "Las Vegas",
+    value: "las-vegas",    
+    id: 8396
+  },
+  "london": {
+    label: "London",
+    value: "london",    
+    id: 24426
+  },
+  "los-angeles": {
+    label: "Los Angeles",
+    value: "los-angeles",    
+    id: 17835
+  },
+  "madrid": {
+    label: "Madrid",
+    value: "madrid",    
+    id: 28755
+  },
+  "milan": {
+    label: "Milan",
+    value: "milan",    
+    id: 30338
+  },
+  "munich": {
+    label: "Munich",
+    value: "munich",    
+    id: 28549
+  },
+  "nashville": {
+    label: "Nashville",
+    value: "nashville",    
+    id: 11104
+  },
+  "new-york": {
+    label: "New York City",
+    value: "new-york",   
+    id: 7644
+  },
+  "paris": {
+    label: "Paris",
+    value: "paris",    
+    id: 28909
+  },
+  "philadelphia": {
+    label: "Philadelphia",
+    value: "philadelphia",    
+    id: 5202
+  },
+  "phoenix": {
+    label: "Phoenix",
+    value: "phoenix",    
+    id: 23068
+  },
+  "portland": {
+    label: "Portland, OR",
+    value: "portland",    
+    id: 12283
+  },
+  "prague": {
+    label: "Prague",
+    value: "prague",    
+    id: 28425
+  },
+  "rome": {
+    label: "Rome",
+    value: "rome",    
+    id: 30366
+  },
+  "san-antonio": {
+    label: "San Antonio",
+    value: "san-antonio",    
+    id: 7554
+  },
+  "san-diego": {
+    label: "San Diego",
+    value: "san-diego",    
+    id: 11086
+  },
+  "san-francisco": {
+    label: "San Francisco",
+    value: "san-francisco",    
+    id: 26330
+  },
+  "seattle": {
+    label: "Seattle",
+    value: "seattle",    
+    id: 2846
+  },
+  "washington-dc": {
+    label: "Washington D.C.",
+    value: "washington-dc",    
+    id: 1409
+  }
+};
+
+export default songkickCityData;
