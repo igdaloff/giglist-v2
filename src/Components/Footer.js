1fd75be6953b03f8d8257937d7ade6b2e6d6bbda
Nathan Igdaloff <louis.nathan@gmail.com>
2022-07-07 10:52:59 -0700

    add Typeform feedback link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export function Footer(){
 	return(
 		<footer className="absolute max-w-4xl left-0 right-0 bottom-0 m-auto p-6 text-s text-gray-400 text-center">
-			<p>Made by <a className="hover:text-white" href="//igdaloff.com">Nathan Igdaloff</a> | <Link to="/about" className="hover:text-white">About</Link></p>			
+			<p>Made by <a className="hover:text-white" href="//igdaloff.com">Nathan Igdaloff</a> | <Link to="/about" className="hover:text-white">About</Link>  |  <a className="hover:text-white" href="https://g09lldp01b5.typeform.com/to/OoHX2k42">Feedback</a></p>			
 		</footer>
 	)
 }
--- src/Routes/about.js
+++ src/Routes/about.js
@@ -7,7 +7,7 @@ export default function About() {
       <p className=""><a href="//igdaloff.com">Nathan Igdaloff</a> built this project in 2022 to learn ReactJS and to encourage people to see live music. It's an iteration of a <a href="https://github.com/jshakes/giglist">previous version</a> built with <a href="https://jshakespeare.com/">James Shakespeare</a>, who also lent a hand with this one.</p>
       <p>Concert data is from the <a href="https://www.songkick.com/developer">Songkick API</a>. Artist query and player embed from the <a href="https://developer.spotify.com/">Spotify API</a>. And most of the icons are from <a href="https://fontawesome.com/">Font Awesome</a>.</p>
       <p>The code is available to view on <a href="https://github.com/igdaloff/giglist-v2">Github</a>.</p>
-      <p>Ideas for improvement? <a href="mailto:louis.nathan@gmail.com?subject=How to make your project less shitty">Email me!</a></p>
+      <p>Ideas for improvement? <a href="https://g09lldp01b5.typeform.com/to/OoHX2k42">Let me know!</a></p>
       <Link className="absolute top-6 left-6 no-underline text-2xl hover:bg-white w-8 h-8 hover:text-black text-center rounded-full" to="/">←</Link>      
 
     </div>    
